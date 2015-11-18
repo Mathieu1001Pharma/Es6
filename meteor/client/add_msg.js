41a0517ab5b7e72591e1b40e71fdef748493f825
mathieu-integre-mac <mathieu@1001pharmacies.com>
2015-11-18 22:49:30 +0100

    [UP] refacto form add msg js

--- meteor/client/add_msg.js
+++ meteor/client/add_msg.js
@@ -1,5 +1,7 @@
 Template.form_add_msg.events({
   'click button': function () {
-    Messages.insert({user: $('#input_user').val(), message: $('#input_message').val() })
+    Messages.insert({
+      user: $('#input_user').val(), message: $('#input_message').val()
+    })
   }
 })
