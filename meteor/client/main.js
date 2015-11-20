7debc7273d2311715e96e6950bb1fb659e633e92
mathieu-integre-mac <mathieu@1001pharmacies.com>
2015-11-20 07:11:33 +0100

    [UP] begun class workaround

--- meteor/client/main.js
+++ meteor/client/main.js
@@ -1,9 +1,31 @@
+class Item {
+  constructor (item) {
+    this.name = item.name
+    this.qty = +item.qty
+    this.price = +item.price
+  }
+}
+
 class Basket {
-  constructor (username) {
+  constructor (firstname) {
     this.items = []
-    this.username = ''
+    this.username = firstname
+  }
+  addItem(item) {
+    let newItem = new Item(item)
+    this.items.push(newItem)
   }
 
 }
 
+basket1 = new Basket('mathieu')
 
+/*
+TODO
+
+addID
+removeItem
+changeQty
+showBaskets
+
+*/
